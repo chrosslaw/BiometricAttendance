e40f596b8aa6a6e54b5959e29fd497aee4ef8cfd
chrosslaw <Chrosslaw@gmail.com>
2025-02-23 13:23:57 -0600

    Add screens for nav locations

---
+++ app/src/main/java/com/example/biometricattendance/ui/HomeScreen.kt
@@ -0,0 +1,20 @@
+package com.example.biometricattendance.ui
+
+import android.content.Context
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+
+@Composable
+fun HomeScreen(navController: NavController, context: Context) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+
+    }
+}
---
+++ app/src/main/java/com/example/biometricattendance/ui/PasswordSetupScreen.kt
@@ -0,0 +1,22 @@
+package com.example.biometricattendance.ui
+
+import android.content.Context
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+
+@Composable
+fun PasswordSetupScreen(navController: NavController, context: Context) {
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+
+    }
+}
